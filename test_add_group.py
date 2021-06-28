805f7ad224e65a6265acf2efba43373d8d6a76f8
shtepkaa <61048714+shtepkaa@users.noreply.github.com>
2021-06-29 00:10:40 +0300

    Задание №4: Перевести тесты с unittest на py.test и реализовать фикстуры

--- test_add_group.py
+++ test_add_group.py
@@ -3,77 +3,24 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+import pytest
 from group import Group
+from application import Application
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
 
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="rfdj", header="pfrohivwri", footer="GOEIPOTHE"))
+    app.logout()
 
-    def open_group_page(self, wd):
-        wd.find_element_by_link_text("groups").click()
 
-    def init_group_creation(self, wd):
-        wd.find_element_by_name("new").click()
-
-    def fill_group_form(self, wd, group):
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").click()
-        # ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=group_footer | ]]
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-
-    def submit_group_creation(self, wd):
-        wd.find_element_by_name("submit").click()
-
-    def return_to_group_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def test_add_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
-        self.init_group_creation(wd)
-        self.fill_group_form(wd, Group(name="rfdj", header="pfrohivwri", footer="GOEIPOTHE"))
-        self.submit_group_creation(wd)
-        self.return_to_group_page(wd)
-        self.logout(wd)
-
-    def test_add_empty_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
-        self.init_group_creation(wd)
-        self.fill_group_form(wd, Group(name="", header="", footer=""))
-        self.submit_group_creation(wd)
-        self.return_to_group_page(wd)
-        self.logout(wd)
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
