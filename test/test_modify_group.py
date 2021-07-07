4f076259a912b8c8b7f92c2cfb1f4ae28392fa66
Olga Shtepa <shtepkaa@gmail.com>
2021-07-07 15:48:17 +0300

    change name

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,7 +1,8 @@
 from model.group import Group
 
 
-def test_delete_first_group(app):
+def test_modify_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="1", header="1", footer="1"))
     app.session.logout()
+
