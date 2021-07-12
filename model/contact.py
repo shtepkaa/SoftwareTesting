199521f49c1fafe118aff95b95d3406e96bf05ab
Olga Shtepa <shtepkaa@gmail.com>
2021-07-12 12:41:50 +0300

    Проверка равенства объектов и списков (для групп и контактов)

--- model/contact.py
+++ model/contact.py
@@ -6,3 +6,9 @@ class Contact:
         self.lastname = lastname
         self.mobile = mobile
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.firstname == other.firstname and self.lastname == other.lastname
