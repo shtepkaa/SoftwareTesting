6e04f73a94b1602ebf8057ff1c17cc2edeb331c2
Olga Shtepa <shtepkaa@gmail.com>
2021-07-12 15:46:59 +0300

    Кеширование при загрузке списков групп

--- fixture/group.py
+++ fixture/group.py
@@ -26,6 +26,7 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
+        self.group_cache = None
 
     def fill_group_form(self, group):
         self.change_field_value("group_name", group.name)
@@ -50,6 +51,7 @@ class GroupHelper:
         except Exception:
             time.sleep(1)
         self.return_to_group_page()
+        self.group_cache = None
 
     def modify_first_group(self, group):
         wd = self.app.wd
@@ -61,6 +63,7 @@ class GroupHelper:
         # submit group update
         wd.find_element_by_name("update").click()
         self.return_to_group_page()
+        self.group_cache = None
 
     def select_first_group(self):
         wd = self.app.wd
@@ -72,15 +75,18 @@ class GroupHelper:
         self.open_group_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+    group_cache = None
+
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_group_page()
-        groups = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name=text, id=id))
-        return groups
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_group_page()
+            self.group_cache = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache)
 
 
 
