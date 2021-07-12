909d74bc768fd186378c3437cc84d77323928136
Olga Shtepa <shtepkaa@gmail.com>
2021-07-12 15:55:11 +0300

    хеширование в тесте с добавлением новой группы

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,12 +5,13 @@ def test_add_group(app):
     old_groups = app.group.get_group_list()
     group = Group(name="rfdj", header="pfrohivwri", footer="GOEIPOTHE")
     app.group.create(group)
+    assert len(old_groups)+1 == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups)+1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
+"""
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
     group = Group(name="", header="", footer="")
@@ -19,3 +20,4 @@ def test_add_empty_group(app):
     assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+"""
