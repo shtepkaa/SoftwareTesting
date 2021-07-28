536916faf5c13662c74a36bfd635fd83c7e44f4e
Olga Shtepa <shtepkaa@gmail.com>
2021-07-28 17:10:27 +0300

    Отношения между объектами

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,12 +1,15 @@
 # from fixture.db import DbFixture
 from fixture.orm import ORMFixture
+from model.group import Group
 
 # db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
     # l = db.get_group_list()
-    l = db.get_contact_list()
+    # l = db.get_contact_list()
+    # l = db.get_contacts_in_group(Group(id='553'))
+    l = db.get_contacts_not_in_group(Group(id='553'))
     for item in l:
         print(item)
     print(len(l))
