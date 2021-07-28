ccfc6268d7e8ae13a2095f7f1f446f7c0e9a2895
Olga Shtepa <shtepkaa@gmail.com>
2021-07-28 17:43:19 +0300

    правка загрузка из json

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -20,11 +20,16 @@ testdata = [
 
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
-    old_contacts = app.contact.get_contact_list()
+def test_add_contact(app, db, contact, check_ui):
+    # old_contacts = app.contact.get_contact_list()
+    old_contacts = db.get_contact_list()
     app.contact.create(contact)
-    assert len(old_contacts)+1 == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
+    # assert len(old_contacts)+1 == app.contact.count()
+    # new_contacts = app.contact.get_contact_list()
+    new_contacts = db.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
+                                                                     key=Contact.id_or_max)
