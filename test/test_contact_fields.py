df138dc911be54bab09c9490e98ab18b4a0cc6f8
shtepkaa <61048714+shtepkaa@users.noreply.github.com>
2021-08-10 19:25:53 +0300

    Задание №21

--- test/test_contact_fields.py
+++ test/test_contact_fields.py
@@ -3,62 +3,19 @@ from random import randrange
 from model.contact import Contact
 
 
-def check_empty_filling(app):
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="figu", lastname="ihfuge", mobile="74575", homephone="65476", workphone="576", secondaryphone="33",
-                      address='jediafhc', email='odfej', email2='frdj', email3='jdfr'))
-
-
-def test_phones_on_home_page(app):
-    check_empty_filling(app)
-    index = randrange(len(app.contact.get_contact_list()))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-
-
-def test_emails_on_home_page(app):
-    check_empty_filling(app)
-    index = randrange(len(app.contact.get_contact_list()))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-
-
-def test_firstname_on_home_page(app):
-    check_empty_filling(app)
-    index = randrange(len(app.contact.get_contact_list()))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-
-
-def test_lastname_on_home_page(app):
-    check_empty_filling(app)
-    index = randrange(len(app.contact.get_contact_list()))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-
-
-def test_address_on_home_page(app):
-    check_empty_filling(app)
-    index = randrange(len(app.contact.get_contact_list()))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.address == contact_from_edit_page.address
-
-
-def test_phones_on_contact_view_page(app):
-    check_empty_filling(app)
-    index = randrange(len(app.contact.get_contact_list()))
-    # если нет элементов H, W и т.д выкидывает, в последующем нужно делать проверку
-    contact_from_view_page = app.contact.get_contact_info_from_view_page(index)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_view_page.homephone == contact_from_edit_page.homephone
-    assert contact_from_view_page.mobile == contact_from_edit_page.mobile
-    assert contact_from_view_page.workphone == contact_from_edit_page.workphone
-    assert contact_from_view_page.secondaryphone == contact_from_edit_page.secondaryphone
+def test_compare_all_contact_info(app, db):
+    contacts_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    contacts_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    assert len(contacts_from_db) == len(contacts_from_home_page)
+    for i in range(len(contacts_from_db)):
+        contact_from_home_page = contacts_from_home_page[i]
+        contact_from_db = contacts_from_db[i]
+        assert contact_from_home_page.id == contact_from_db.id
+        assert contact_from_home_page.firstname == contact_from_db.firstname
+        assert contact_from_home_page.lastname == contact_from_db.lastname
+        assert contact_from_home_page.address == contact_from_db.address
+        assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_db.all_phones_from_home_page)
+        assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_db)
 
 
 def clear(s):
